50b74768c5773eae422e24bbee8b1d869a76cbc8
Dana <dana@hlavacova.de>
2023-05-29 21:02:43 +0200

    f

---
+++ src/componenten/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {FaComment}from 'react-icons/fa';
+import './Header.scss'
+const Header = () => {
+    return (
+        <div>
+
+            <h2 className="title">🗨 Teile deine Meinung zu diesem Artikel!</h2>
+        </div>
+    );
+};
+
+export default Header;
